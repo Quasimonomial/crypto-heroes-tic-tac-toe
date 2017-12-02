c82775eeff959b56239832a031c7fc9524bc1485
Travis Herrick <quasimonomial@gmail.com>
2017-12-01 21:10:49 -0800

    add gamestate/board

--- contracts/TicTacToe.sol
+++ contracts/TicTacToe.sol
@@ -1,7 +1,13 @@
 pragma solidity ^0.4.4;
 
 contract TicTacToe {
+  uint[9] gameBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+	// Represents game board, first 3 are top row, next 3 are middle row, last three are bottom row
+	// 0 represents a blank state, 1 represets an X, 2 represents an O.  X plays first
+
+
+	// Runs on Contract initilization
 	function TicTacToe() {
-		
+
 	}
 }
