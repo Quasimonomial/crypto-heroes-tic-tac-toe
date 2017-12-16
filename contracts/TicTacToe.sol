360ad5cad7c97ac8c2c445f7bc837ff68a712a1e
Travis Herrick <quasimonomial@gmail.com>
2017-12-16 11:58:38 -0800

    use addressed to hold moves

--- contracts/TicTacToe.sol
+++ contracts/TicTacToe.sol
@@ -1,10 +1,18 @@
 pragma solidity ^0.4.4;
 
 contract TicTacToe {
-
+  // Represents Game State in 3 x 3 grid.  Your address represents your moves
+  address[3][3] public board;
 
 	// Runs on Contract initilization
-	function TicTacToe() {
-
+	function TicTacToe () {
 	}
+
+  function makeMove(uint8 x, uint8 y) {
+    board[x][y] = msg.sender;
+
+    MadeMove(true);
+  }
+
+  event MadeMove(bool);
 }
