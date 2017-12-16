9680f2ed9b5d6f2e445c5545cc75e2774719b2b2
Travis Herrick <quasimonomial@gmail.com>
2017-12-15 20:19:13 -0800

    delete things

--- contracts/TicTacToe.sol
+++ contracts/TicTacToe.sol
@@ -1,14 +1,7 @@
 pragma solidity ^0.4.4;
 
 contract TicTacToe {
-  uint[9] gameBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-	// Represents game board, first 3 are top row, next 3 are middle row, last three are bottom row
-	// 0 represents a blank state, 1 represets an X, 2 represents an O.  X plays first
 
-
-	function makeMove(uint piece, uint8 posistion) public {
-		gameBoard[posistion] = piece;
-	}
 
 	// Runs on Contract initilization
 	function TicTacToe() {
